6e9a170b12927003a482132d8c307a807ae62cc7
lvhaidong <haidonglu@126.com>
2019-08-08 08:35:20 +0800

    2019-08-07

--- springbootmvcwebmybatis/src/main/java/com/neusoft/busmis/SpringbootmvcwebmybatisApplication.java
+++ springbootmvcwebmybatis/src/main/java/com/neusoft/busmis/SpringbootmvcwebmybatisApplication.java
@@ -2,8 +2,12 @@ package com.neusoft.busmis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySource(value="classpath:jdbc.properties")
+@PropertySource(value="classpath:my.properties")
 public class SpringbootmvcwebmybatisApplication {
 
 	public static void main(String[] args) {
