6eda45276cf262f136bb648deb2f4766604de093
haidonglu9001 <haidonglu@126.com>
2019-08-18 13:47:29 +0800

    2019-08-18下午修改RoleService实现类

--- NeusoftOAHRServiceLHD/src/main/java/com/neusoft/oa/hrservice/service/impl/RoleServiceImpl.java
+++ NeusoftOAHRServiceLHD/src/main/java/com/neusoft/oa/hrservice/service/impl/RoleServiceImpl.java
@@ -1,16 +1,19 @@
-package com.neusoft.oa.hrservice.service;
+package com.neusoft.oa.hrservice.service.impl;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.neusoft.oa.hrservice.mapper.IRoleMapper;
 import com.neusoft.oa.hrservice.model.RoleModel;
+import com.neusoft.oa.hrservice.service.IRoleService;
 
 @Service
 @Transactional
+
 public class RoleServiceImpl implements IRoleService {
 	@Autowired
 	private IRoleMapper roleMapper=null;
