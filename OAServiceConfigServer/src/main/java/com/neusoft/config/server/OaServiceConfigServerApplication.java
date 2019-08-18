4c234fa8c00555d436123d3874da0d7b09f2de04
haidonglu9001 <haidonglu@126.com>
2019-08-18 16:59:14 +0800

    2019-08-18下午下课前提交

---
+++ OAServiceConfigServer/src/main/java/com/neusoft/config/server/OaServiceConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.neusoft.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class OaServiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OaServiceConfigServerApplication.class, args);
+	}
+
+}
